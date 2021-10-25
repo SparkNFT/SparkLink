ac109b573acfcb3ca45a6550d7e33dbab895e82a
qiaoao-spec <1304523405@qq.com>
2021-10-25 17:40:39 +0800

    更改account的获取方式

--- src/TopBar.js
+++ src/TopBar.js
@@ -339,7 +339,7 @@ class TopBar extends Component {
         this.setState({
           isConnected: true
         });
-        this.setState({userAddress:account});
+        this.setState({ userAddress: account });
         //localStorage.setItem(USERADDRESS, account);
         localStorage.setItem(LASTCONNECT, METAMASK)
       }
@@ -364,7 +364,7 @@ class TopBar extends Component {
           isConnected: true
         });
         //localStorage.setItem(USERADDRESS, account);
-        this.setState({userAddress:account});
+        this.setState({ userAddress: account });
         localStorage.setItem(LASTCONNECT, TOKENPOCKET);
       }
 
@@ -428,30 +428,43 @@ class TopBar extends Component {
 
   //获取TokenPocket用户地址
   getTokenPocketAccount = async () => {
-    try {
-      //todo 使用TP链接
-      let account;
-      await tp.getWallet({ walletTypes: ['matic'], switch: false }).then(
-        value => {
-          account = value.data.address;
-        }
-      )
-      alert('您已经连接tokenpocket, 当前账户： ' + account)
-      this.setState({ isConnected: true, });
-      this.setState({ userAddress: account });
-      //localStorage.setItem(USERADDRESS, account); //储存用户address
-      localStorage.setItem(LASTCONNECT, TOKENPOCKET); //储存上次登陆的信息
-    } catch (error) {
-      console.debug(error);
-      this.setState({ isConnected: false });
+    if (this.state.isConnected) {
+      alert('您已经连接tokenpocket, 当前账户： ' + this.state.userAddress)
     }
+    else {
+      try {
+        //todo 使用TP链接
+        let account;
+        await tp.getWallet({ walletTypes: ['matic'], switch: false }).then(
+          value => {
+            account = value.data.address;
+          }
+        )
+        alert('您已经连接tokenpocket, 当前账户： ' + account)
+        this.setState({ isConnected: true, });
+        this.setState({ userAddress: account });
+        //localStorage.setItem(USERADDRESS, account); //储存用户address
+        localStorage.setItem(LASTCONNECT, TOKENPOCKET); //储存上次登陆的信息
+      } catch (error) {
+        console.debug(error);
+        this.setState({ isConnected: false });
+      }
+    }
+
+  }
+
+  //登陆后点击token按钮
+  handleTokenButtonOnClick = () => {
+    let string = localStorage.getItem(LASTCONNECT);
+    if (string === METAMASK) this.getMetaMaskAccount();
+    else if (string === TOKENPOCKET) this.getTokenPocketAccount();
   }
 
   //logout
   //todo 未做登出
   disconnect = () => {
     this.setState({ isConnected: false });
-    localStorage.removeItem(USERADDRESS);
+    //localStorage.removeItem(USERADDRESS);
     localStorage.removeItem(LASTCONNECT);
 
   }
@@ -515,13 +528,11 @@ class TopBar extends Component {
                 // <Button onClick={this.getAccount}>
                 //   <WalletTwoTone className={classes.icon} />
                 // </Button>
-                <Button size="large" variant="contained" className={classes.btnUser} onClick={this.getMetaMaskAccount}>
+                <Button size="large" variant="contained" className={classes.btnUser} onClick={this.handleTokenButtonOnClick}>
                   <Typography component="" color="inherit" noWrap className={classes.titleToken}>
                     {this.state.userAddress.substring(0, 6)}...{this.state.userAddress.substring(this.state.userAddress.length - 5, this.state.userAddress.length)}
                   </Typography>
-
                 </Button>
-
               ) : (
                 // <Button onClick={this.getAccount}>
                 //   <WalletFilled className={classes.icon} />
@@ -532,10 +543,11 @@ class TopBar extends Component {
                   </Typography>
                 </Button>
               )}
+              {/* <Button onClick={this.disconnect}> akdalk</Button> */}
             </Grid>
           </Grid>
         </Toolbar>
-      </div>
+      </div >
     );
   }
 }
