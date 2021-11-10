b0457028aa1500f51ed146dd7c55c8debba2908e
yuhuison <yuhuison@live.com>
2021-11-10 23:58:47 +0800

    Update ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom'
-import 'moment/locale/zh-cn';
 class ScrollToTop extends Component {
 	componentDidUpdate(prevProps) {
 		if (this.props.location.pathname !== prevProps.location.pathname) {
