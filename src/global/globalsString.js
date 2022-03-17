7789cd23c1be1b6be7e09a8d88a07f8f217bedbc
codeAtNow <moonsky862@gmail.com>
2022-03-17 20:29:04 +0800

    feat(empty): clear old files

--- src/global/globalsString.js
+++
@@ -1,8 +0,0 @@
-//字符串常量
-const TOKENPOCKET = 'TokenPocket';
-const METAMASK = 'MetaMask';
-const LASTCONNECT = 'lastConnect';
-const USERADDRESS = 'userAddress';
-const MATHWALLET = 'MathWallet';
-
-export {TOKENPOCKET, METAMASK, LASTCONNECT ,USERADDRESS, MATHWALLET}; 
--- src/utils/isMobile.js
+++
@@ -1,9 +0,0 @@
-//判断当前设备是否为移动设备
-var isMobile;
-let userAgent = navigator.userAgent.toLowerCase();
-if (/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(userAgent)) {
-	isMobile = true;
-} else {
-	isMobile = false;
-}
-export default isMobile;
