d837ba940385ed8f01140b16c458e0818d97cc34
Santana <104019956+SantanaSP@users.noreply.github.com>
2022-05-18 00:39:47 +0900

    refactor: optimize scripts and add +x permission.

--- packages/site/scripts/github-pages/production.sh
+++ packages/site/scripts/github-pages/production.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+set -e
 BASEDIR=$(dirname "$0")
 $BASEDIR/push.sh SparkNFT/Frontend.production.page.git sparklink.io $1
--- packages/site/scripts/github-pages/staging.sh
+++ packages/site/scripts/github-pages/staging.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+set -e
 BASEDIR=$(dirname "$0")
 $BASEDIR/push.sh SparkNFT/Frontend.page.git app-staging.sparklink.io $1
